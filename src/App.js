87c97a3cdb5440130d0813acd45c369adaed7f63
myname <email@gmail.com>
2021-06-02 20:17:12 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  Typography,
+  Grid,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CssBaseline,
+  Toolbar,
+  Container,
+  Button,
+} from '@material-ui/core'
+import PhotoCameraIcon from '@material-ui/icons/PhotoCamera'
+import useStyles from './styles'
 
 function App() {
+  const classes = useStyles()
+
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <CssBaseline />
+      <AppBar position='relative'>
+        <Toolbar>
+          <PhotoCameraIcon className={classes.icon} />
+          <Typography variant='h6'>Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth='sm'>
+            <Typography
+              variant='h2'
+              align='center'
+              color='textPrimary'
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography
+              variant='h5'
+              align='center'
+              color='textSecondary'
+              gutterBottom
+            >
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Assumenda quae magnam in perspiciatis. Similique, error! Sunt,
+              exercitationem illum. Architecto dicta repudiandae, in
+            </Typography>
+            <div className={classes.buttons}>
+              <Grid container spacing={2} justify='center'>
+                <Grid item>
+                  <Button variant='contained' color='primary'>
+                    see me photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant='outlined' color='primary'>
+                    see me photos
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth='lg'>
+          <Grid container spacing={3}>
+            {cards.map((card) => {
+              return (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card className={classes.card}>
+                    <CardMedia
+                      className={classes.cardMedia}
+                      image='https://images.unsplash.com/photo-1596920566403-2072ed25d7f5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+                      title='image title'
+                    />
+                    <CardContent className={classes.cardContent}>
+                      <Typography gutterBottom variant='h5'>
+                        Heading
+                      </Typography>
+                      <Typography>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Eveniet, voluptatum?
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size='small' color='primary'>
+                        View
+                      </Button>{' '}
+                      <Button size='small' color='primary'>
+                        Edit
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              )
+            })}
+          </Grid>
+        </Container>
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant='h6' align='center' gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant='subtitle1' align='center' color='textSecondary'>
+          Lorem ipsum dolor sit amet consectetur adipisicing.
+        </Typography>
+      </footer>
+    </>
+  )
 }
 
-export default App;
+export default App
